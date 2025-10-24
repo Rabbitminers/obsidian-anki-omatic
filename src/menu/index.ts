1aa6072529714b035482467fca1783e386adfd10
Rabbitminers <rabbitminers2.0@gmail.com>
2025-10-24 13:55:57 +0100

    feat: display toast when trying to export empty deck

--- src/menu/index.ts
+++ src/menu/index.ts
@@ -1,14 +1,19 @@
-import { Menu, TAbstractFile, } from "obsidian";
+import { App, Menu, Notice, TAbstractFile, } from "obsidian";
 import { exportQuestionsToCsv } from "src/anki/csv";
 import { convertAbstractFileToQuestions } from "src/anki/export";
 import { trimExtension } from "src/file/filename";
 
-export function handleFileMenu(menu: Menu, file: TAbstractFile, source: string) {
+export function handleFileMenu(menu: Menu, file: TAbstractFile, app: App) {
 	const handleClick = async () => {
 		const questions = await convertAbstractFileToQuestions(file);
 		const filename = trimExtension(file.name);
 
-		await exportQuestionsToCsv(questions, filename);
+		if (questions.length === 0) {
+			new Notice("No flashcards found here");
+			return;
+		}
+
+		await exportQuestionsToCsv(questions, filename, app);
 	}
 
 	menu.addItem(item => {
